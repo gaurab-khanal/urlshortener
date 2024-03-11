9f3683a16113faa04b4fefe4b00b79ed7a536609
Gaurab Khanal <khanalgaurab98@gmail.com>
2024-03-11 18:16:13 +0545

    responsive

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -31,54 +31,56 @@ export default function Home() {
   };
 
   return (
-    <div className="h-screen bg-indigo-900 flex justify-center">
-      <div className="flex justify-center lg:items-center  lg:gap-[20px] lg:flex-col">
-        <div className="text-white text-2xl font-bold ">URL shortener </div>
-        <div className="flex flex-col gap-4 ml-4 lg:mt-0 mt-[30%] ">
-          <form
-            action={handleClick}
-            className="flex lg:flex-row flex-col gap-4 items-center"
+    <div className="h-screen bg-indigo-900 flex flex-col justify-center items-center">
+      <div className="text-white text-2xl font-bold  absolute top-4 ">
+        URL shortener{" "}
+      </div>
+      <div className=" ">
+        <form
+          action={handleClick}
+          className="flex lg:flex-row flex-col gap-4 items-center"
+        >
+          <input
+            type="text"
+            name="url"
+            placeholder="Enter url"
+            className="h-[30px] rounded-lg mr-[20px] outline-none p-2 text-center font-normal text-large border-2"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full"
           >
-            <input
-              type="text"
-              name="url"
-              placeholder="Enter url"
-              className="h-[30px] rounded-lg mr-[20px] outline-none p-2 text-center font-normal text-large border-2"
-            />
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full"
-            >
-              {loading ? (
-                <TailSpin
-                  visible={true}
-                  height="20"
-                  width="20"
-                  color="#ffffff"
-                  ariaLabel="tail-spin-loading"
-                  radius="1"
-                  wrapperStyle={{}}
-                  wrapperClass=""
-                />
-              ) : (
-                "Submit"
-              )}
-            </button>
-          </form>
-          {shortUrl && (
-            <>
-              <div className="flex lg:flex-row flex-col gap-4 items-center">
-                <div className="flex items-center rounded-lg mr-[20px]  outline-none p-2 text-center font-normal text-large border-2  bg-white">
-                  {shortUrl}
-                </div>
-                <button
-                  onClick={copyPasswordToClipBoard}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full "
-                >{copyText}</button>
+            {loading ? (
+              <TailSpin
+                visible={true}
+                height="20"
+                width="20"
+                color="#ffffff"
+                ariaLabel="tail-spin-loading"
+                radius="1"
+                wrapperStyle={{}}
+                wrapperClass=""
+              />
+            ) : (
+              "Submit"
+            )}
+          </button>
+        </form>
+        {shortUrl && (
+          <>
+            <div className="flex lg:flex-row flex-col gap-4 items-center mt-4">
+              <div className="flex items-center rounded-lg mr-[20px]  outline-none p-2 text-center font-normal text-large border-2  bg-white">
+                {shortUrl}
               </div>
-            </>
-          )}
-        </div>
+              <button
+                onClick={copyPasswordToClipBoard}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full "
+              >
+                {copyText}
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
